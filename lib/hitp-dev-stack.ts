df4db7463a722d31ac11b1ed44da25c80a9f8821
Ramil <rdiongco@gmail.com>
2023-08-16 18:16:53 +0800

    change token

--- lib/hitp-dev-stack.ts
+++ lib/hitp-dev-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { PipelineAppStage } from './pipeline-app-stack';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { SecretValue, Stack, StackProps } from 'aws-cdk-lib/core';
+
 
 
 export class HitpDevStack extends cdk.Stack {
@@ -12,7 +14,10 @@ export class HitpDevStack extends cdk.Stack {
     const hitppipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'HITPPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Arahan888/hitp-dev', 'main'),
+        input: CodePipelineSource.gitHub('Arahan888/hitp-dev', 'main', {authentication: SecretValue.secretsManager('devgithub', {
+          jsonField: 'devgithub'
+        })}),
+        
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
