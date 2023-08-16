9883e07636ce31a9d10ea318d8e77765a3f49ee9
Ramil <rdiongco@gmail.com>
2023-08-16 14:27:14 +0800

    change lambda to demo

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -16,17 +16,22 @@ export class lambdaStack extends cdk.Stack {
     //     //vpc:getExistingVpc
     //   });
   
-  
-      const lambdaretrievevitals = new lambda.Function(this, 'lambdaretrievevitalsid', {
-        handler:'lambda_retrievevitals.retrievevitals',
-        runtime: lambda.Runtime.PYTHON_3_11,
-        code: lambda.Code.fromAsset('./services/'),
-        functionName: 'lambdaretrievevitals',
-        //handler: 'index.handler',
-        //code: lambda.Code.fromInline('exports.handler = _ => "Hello, CDK";')
-        //role: lambdaVPCExecutionRole,
-        //vpc:getExistingVpc
-      });
+    const demolambda = new lambda.Function(this, 'LambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline('exports.handler = _ => "Hello, CDK";')
+    });
+
+      // const lambdaretrievevitals = new lambda.Function(this, 'lambdaretrievevitalsid', {
+      //   handler:'lambda_retrievevitals.retrievevitals',
+      //   runtime: lambda.Runtime.PYTHON_3_11,
+      //   code: lambda.Code.fromAsset('./services/'),
+      //   functionName: 'lambdaretrievevitals',
+      //   //handler: 'index.handler',
+      //   //code: lambda.Code.fromInline('exports.handler = _ => "Hello, CDK";')
+      //   //role: lambdaVPCExecutionRole,
+      //   //vpc:getExistingVpc
+      // });
 
     }
 }     
