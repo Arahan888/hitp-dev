10bad4fdd40bc35dd4c826ff545d64403f6a37c4
Ramil <rdiongco@gmail.com>
2023-08-20 21:24:12 +0800

    fromlookup vpc

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as cdkcore from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as lambda from  'aws-cdk-lib/aws-lambda';
 import * as ec2 from  'aws-cdk-lib/aws-ec2';
@@ -22,14 +23,43 @@ export class lambdaStack extends cdk.Stack {
     //     functionName: 'testlambda',
     //     //vpc:getExistingVpc
     //   });
-   // const getExistingVpc = ec2.Vpc.fromLookup(this, `VPC`, { vpcName: `HITP-test` });
+   const getExistingVpc = ec2.Vpc.fromLookup(this, `VPC-${stagename}`, { vpcName: `HITP-test` });
+  
+//    const getExistingTestVpc = ec2.Vpc.fromVpcAttributes(this, 'HITP-test', {
+//     vpcId:'vpc-0b828649733458d5a',
+//     availabilityZones: ['ap-southeast-1a','ap-southeast-1b','ap-southeast-1c'],
+//    // publicSubnetIds: ['subnet-0183c2e0f5154e250','subnet-0a434094a9bbfe3b3','subnet-0bc0da1e916e56521']
+//     privateSubnetIds: ['subnet-030dc571a75a456fe','subnet-0cb1831be44095ed8','subnet-008b54d4205875805']
+// });
 
-   const getExistingVpc = ec2.Vpc.fromVpcAttributes(this, 'HITP-test', {
-    vpcId:'vpc-0b828649733458d5a',
-    availabilityZones: ['ap-southeast-1a','ap-southeast-1b','ap-southeast-1c'],
-   // publicSubnetIds: ['subnet-0183c2e0f5154e250','subnet-0a434094a9bbfe3b3','subnet-0bc0da1e916e56521']
-    privateSubnetIds: ['subnet-030dc571a75a456fe','subnet-0cb1831be44095ed8','subnet-008b54d4205875805']
-});
+
+// const getExistingProdVpc = ec2.Vpc.fromVpcAttributes(this, 'HITP-prod', {
+//   vpcId:'vpc-0632cc682151dc451',
+//   availabilityZones: ['ap-southeast-1a','ap-southeast-1b','ap-southeast-1c'],
+//  // publicSubnetIds: ['subnet-0183c2e0f5154e250','subnet-0a434094a9bbfe3b3','subnet-0bc0da1e916e56521']
+//   privateSubnetIds: ['subnet-0899e8ad5dfd40e1b','subnet-04b7d85280c5a1700','subnet-068db935ab09e8e4d']
+// });
+
+   new cdkcore.CfnCondition(this, "UseProdVPC", {
+    expression: cdk.Fn.conditionEquals(stagename, "prod")
+  });
+  
+  // const bucketName = cdkcore.Fn.conditionIf(
+  //   "UseProdVPC",
+  //   const prodVPC = getExistingVpc,
+  //   "app-beta-bucket"
+  // );
+  
+  // const Bucket = s3.Bucket.import(this, "AppBucket", {
+  //   bucketName
+  // });
+
+
+
+
+   
+
+
 
     // const demolambda = new lambda.Function(this, 'LambdaFunction', {
     //   runtime: lambda.Runtime.NODEJS_14_X,
@@ -53,7 +83,8 @@ export class lambdaStack extends cdk.Stack {
         code: lambda.Code.fromAsset('./services/'),
         functionName: `lambdaretrievevitals-${stagename}`,
         role: lambdaVPCExecutionRole,
-        vpc:getExistingVpc,
+       // vpc:getExistingTestVpc,
+       vpc: getExistingVpc,
        // allowPublicSubnet:true,
         environment: {
           ENV: stagename,
